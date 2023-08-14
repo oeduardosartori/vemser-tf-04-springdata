db76ac1cd9edabb791258afce14cd93db9cc705f
Eduardo <edusartori00@gmail.com>
2023-08-14 20:52:56 -0300

    Atualizado - ClienteRepository

--- src/main/java/br/com/dbc/vemser/ecommerce/repository/ClienteRepository.java
+++ src/main/java/br/com/dbc/vemser/ecommerce/repository/ClienteRepository.java
@@ -4,4 +4,5 @@ import br.com.dbc.vemser.ecommerce.entity.ClienteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<ClienteEntity, Integer> {
+
 }
