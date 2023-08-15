7c7606e64cee96c80502e0d6158d78ee8dcb46c7
Eduardo <edusartori00@gmail.com>
2023-08-14 21:43:14 -0300

    Atualizado - EnderecoController

--- src/main/java/br/com/dbc/vemser/ecommerce/entity/ClienteEntity.java
+++ src/main/java/br/com/dbc/vemser/ecommerce/entity/ClienteEntity.java
@@ -1,33 +1,48 @@
 package br.com.dbc.vemser.ecommerce.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "cliente")
-public class ClienteEntity {
+@Entity(name = "CLIENTE")
+public class Cliente {
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CLIENTE_SEQ")
-    @SequenceGenerator(name = "CLIENTE_SEQ", sequenceName = "seq_cliente", allocationSize = 1)
-    @Column(name = "id_cliente")
+    @SequenceGenerator(name = "CLIENTE_SEQ", sequenceName = "SEQ_CLIENTE", allocationSize = 1)
+    @Column(name = "ID_CLIENTE")
     private Integer idCliente;
 
-    @Column(name = "nome")
+    @Column(name = "NOME")
     private String nome;
 
-    @Column(name = "telefone")
+    @Column(name = "TELEFONE")
     private String telefone;
 
-    @Column(name = "email")
+    @Column(name = "EMAIL")
     private String email;
 
-    @Column(name = "cpf")
+    @Column(name = "CPF")
     private String cpf;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<EnderecoEntity> enderecoEntities = new HashSet<>();
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<PedidoEntity> pedidoEntities;
+
+    public void addEndereco(EnderecoEntity enderecoEntity) {
+        enderecoEntities.add(enderecoEntity);
+    }
+
 }
