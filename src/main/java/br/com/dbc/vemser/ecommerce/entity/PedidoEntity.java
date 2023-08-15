afb00da24baa894b13029be2cba5cd1ec3872e01
Eduardo <edusartori00@gmail.com>
2023-08-14 21:46:55 -0300

    Atualização - rename ClienteEntity

--- src/main/java/br/com/dbc/vemser/ecommerce/entity/PedidoEntity.java
+++ src/main/java/br/com/dbc/vemser/ecommerce/entity/PedidoEntity.java
@@ -23,7 +23,7 @@ public class PedidoEntity {
     @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_cliente", referencedColumnName = "id_cliente")
-    private Cliente cliente;
+    private ClienteEntity cliente;
 
     @Column(name = "valor")
     private Double valor;
